27d513a4a2f4f5a3c9163d36115fb2cfc9c152e3
nitharwalme77 <nitharwalme77@users.noreply.github.com>
2023-05-16 13:47:45 +0530

    sleuth dependency

--- src/main/java/com/example/accounts/controller/AccountsController.java
+++ src/main/java/com/example/accounts/controller/AccountsController.java
@@ -9,17 +9,18 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class AccountsController {
+    private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
 
     @Autowired
     private AccountsRepository accountsRepository;
@@ -62,7 +63,9 @@ public class AccountsController {
      * ="myCustomerDetailsFallBack")
      */
    // @Retry(name = "retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallBack")
-    public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+    public CustomerDetails myCustomerDetails(@RequestHeader("eazybank-correlation-id") String correlationid, @RequestBody Customer customer) {
+        logger.info("myCustomerDetails() method started");
+
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
         List<Loans> loans = loansFeignClient.getLoansDetails(customer);
         List<Cards> cards = cardsFeignClient.getCardDetails(customer);
@@ -71,12 +74,13 @@ public class AccountsController {
         customerDetails.setAccounts(accounts);
         customerDetails.setLoans(loans);
         customerDetails.setCards(cards);
+        logger.info("myCustomerDetails() method ended");
 
         return customerDetails;
 
     }
 
-    private CustomerDetails myCustomerDetailsFallBack(Customer customer, Throwable t) {
+    private CustomerDetails myCustomerDetailsFallBack(@RequestHeader("eazybank-correlation-id") String correlationid,Customer customer, Throwable t) {
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
         List<Loans> loans = loansFeignClient.getLoansDetails(customer);
         CustomerDetails customerDetails = new CustomerDetails();
@@ -86,4 +90,13 @@ public class AccountsController {
 
     }
 
+    @GetMapping("/sayHello")
+    @RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallback")
+    public String sayHello() {
+        return "Hello, Welcome to EazyBank";
+    }
+
+    private String sayHelloFallback(Throwable t) {
+        return "Hi, Welcome to EazyBank";
+    }
 }
