5951083adb0f3ac44782f44c4068811644c7538f
nitharwalme77 <nitharwalme77@users.noreply.github.com>
2023-05-17 09:46:43 +0530

    health matrices micrometer promethious

--- src/main/java/com/example/accounts/AccountsApplication.java
+++ src/main/java/com/example/accounts/AccountsApplication.java
@@ -1,9 +1,12 @@
 package com.example.accounts;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -21,4 +24,9 @@ public class AccountsApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountsApplication.class, args);
     }
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
+
 }
